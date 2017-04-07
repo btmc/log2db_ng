5cd1087ba17a00193f2f2ea078698256ad87c717
lab79 <lab79@users.noreply.github.com>
2017-04-07 11:13:18 +0300

    Update log2db_ng_player_events.py

--- log2db_ng_player_events.py
+++ log2db_ng_player_events.py
@@ -79,7 +79,7 @@ def parse_args():
 				dest    = "dbdatabase", 
 				default = "stats",
 				help    = "db name")
-
+	
 	parser.add_option("-s", "--sampling_data", 
 				type    = "string", 
 				dest    = "sampling_data", 
@@ -87,6 +87,7 @@ def parse_args():
 									# example "cid:" 		=> 1) cid is mandatory 2) value does not matter
 				help    = "sampling_data")	
   
+
 	(prog_options, prog_args) = parser.parse_args()
 
 	return (prog_options, prog_args)
@@ -102,13 +103,21 @@ class UploadSession(object):
 		self.filename = filename 
 		self.data_type = data_type
 		self.sampling_data = sampling_data
+		
+		self.sampling_data_field = ''
+		self.sampling_data_mask = ''
+		sampling_data_splited = sampling_data.split(':')
+		self.sampling_data_field = sampling_data_splited[0] # re.split('[:]', sampling_data) # self.sampling_data = sampling_data
+
+		self.sampling_data_mask = sampling_data_splited[1]
+		self.sampling_data_mask_len = len(self.sampling_data_mask)
 
 		super(UploadSession, self).__init__(*args, **kwargs)
 
 	def __enter__(self):
 		for subclass in self.__class__.__subclasses__():
 			if self.data_type in subclass.data_types:
-				return subclass(self.filename, self.data_type, self.sampling_data) 
+				return subclass(self.filename, self.data_type, self.sampling_data) # return subclass(self.filename, self.data_type, self.sampling_data_field, self.sampling_data_mask) # 
 
 		raise TypeError()
 
@@ -119,7 +128,6 @@ class UploadSession(object):
 		self.pgsql_conn_cursor.close()
 
 	def open(self):
-	   
 		self.pgsql_conn_cursor.execute  ( \
 										   'insert into \
 												upload_session    ( \
@@ -140,19 +148,17 @@ class UploadSession(object):
 
 		return self.pgsql_conn_cursor.fetchone()[0]
  
-	def parse_line(self, line, sampling_data_field, sampling_data_mask):
+	def parse_line(self, line): # , sampling_data_field, sampling_data_mask
 		raise NotImplementedError()
 
-	def parse(self):
+	def parse(self):		
 		self.session_id = self.open()
-
 		log_file = open(self.filename, 'r')
 		tmp_file = tempfile.TemporaryFile()
 		error_file = tempfile.NamedTemporaryFile(dir=os.path.dirname(self.filename))
-		print(log_file)
+		#print(log_file)
 		self.rows_processed = 0
 		self.rows_prepared = 0
-		self.sampling_data_field, self.sampling_data_mask = re.split('[:]', sampling_data)
 
 		for line in log_file:
 			try:
@@ -160,10 +166,11 @@ class UploadSession(object):
 
 				line = line.strip()
 
-				facts = self.parse_line(line, self.sampling_data_field, self.sampling_data_mask)
+				facts = self.parse_line(line) # , self.sampling_data_field, self.sampling_data_mask)
 				# facts = {k:v.clean() for k,v in facts.iteritems()}
 
-				tmp_file.write('\t'.join((str(self.session_id), json.dumps(facts).encode('string-escape'),)) + '\n')
+				if facts != None:
+					tmp_file.write('\t'.join((str(self.session_id), json.dumps(facts).encode('string-escape'),)) + '\n')
 
 				self.rows_prepared += 1
 
@@ -254,7 +261,6 @@ class UploadSession(object):
 												'session_id':       self.session_id, \
 											} \
 										)
-
 		self.pgsql_conn.commit() 
 
 		log_file.close()
@@ -265,10 +271,14 @@ class UploadSession(object):
 
 		error_file.close()
 
+
+
 class UploadSessionPlayerEvents(UploadSession):
 	data_types = ['player_events', 'player_events_test', 'player_events_v5',]
 
-	anonymous_fields = ('rts', 'ip',)
+	anonymous_fields_server = ('rts', 'ip',)
+	# 1489616707.108|84.163.88.44|4.3|A0982149-7FEB-E6EA-EB4F-934DB1F7DCD5||1E2A36EE-A425-8537-D22F-05B52E31AAD9|1|2||F|PLADF||101964442|||https://cdn.pladform.ru|||M|PLDF_PL:18046|ADXREF:https%3A%2F%2Fpubads.g.doubleclick.net%2Fgampad%2Fads%3Fsz%3D640x480%26iu%3D%2F98544404%2FPladformNetwork%26cust_params%3Dcllive%253D2%2526clps%253D3%2526clurl%253D%25%25PATTERN%3Aodnoklassniki.ru%2526clcontentID%253D4m%2526clint%253Dwaterfall%2526clviti%253D%25%25PATTERN%3Avideotitle%25%25%2526clvidesc%253D%25%25PATTERN%3Avideodescription%25%25%2526clcoin%253D1%2526clcoty%253Dnonesl%2526clcogrp%253Dgames%2526clauone%253DPladformNetwork%2526clpos%253Dpre%2526cldevca%253Ddesktop%2526clvimu%253D2%2526clvpaid%253D1%26impl%3Ds%26gdfp_req%3D1%26env%3Dvp%26output%3Dvast%26unviewed_position_start%3D1%26url%3Dodnoklassniki.ru%26description_url%3Dodnoklassniki.ru%26correlator%3D9769302930355203|ADVXL:0|ADREF:nobanner|ADVPT:2298|ADCLS:0|ADWRPC:0
+	anonymous_fields_client = ('log_version', 'cid', 'uid', 'pid', 'mid', 'n', 'q', 'app_type', 'a', 'content_type', 'content_id', 'stream_server_id', 'hd', 'edge', 'bitrate', 'paid_status', 'video_format') 
 
 	def __init__(self, filename, data_type, sampling_data, *args, **kwargs):
 		super(UploadSessionPlayerEvents, self).__init__(filename, data_type, sampling_data, *args, **kwargs)
@@ -291,43 +301,65 @@ class UploadSessionPlayerEvents(UploadSession):
 										)
 
 		self.fields = self.pgsql_conn_cursor.fetchall()
-		self.sampling_data = sampling_data
-
+		
 		self.mandatory_fields = set(x[0] for x in filter(lambda x: x[3], self.fields))
 
 
-	def parse_line(self, line, sampling_data_field, sampling_data_mask):
-		def line_to_fields(line):
-			return \
-				{i.groupdict()['id'].lower():i.groupdict()['value'] for i in re.finditer('(?i)(?<=[|])(?P<id>[a-z0-9_-]{,32}):(?P<value>[^|]*)', '|' + line)}
+	def parse_line(self, line): # , sampling_data_field, sampling_data_mask
+		line1 = line.lower()
+		split1 = line1.split('|')
+		# print(split1)
 
-		raw_fields = \
-			collections.defaultdict \
-			( \
-				lambda: None, \
-				line_to_fields(line) \
-			)
-
-		raw_fields.update(zip(self.anonymous_fields, re.split('[|]', line)))
+		if len(split1)<3:
+			print(line1)
+			raise NameError('len(split1)<3') 
+		elif len(split1)==3: # 1489616704.137|46.38.48.106|4.3%7C085049C7-8889-9F9F-1424-62F5B6E53B2B%7C%7C2A9328B8-BBCB-E4C1-3585-406573454A6A%7C5%7C132%7C%7CF%7CPLADF%7C%7C100821058%7C%7C%7Chttp://cdn.pladform.ru%7C%7C%7CM%7CPLDF_PL:13151%7CADXREF:http%3A%2F%2Fads.adfox.ru%2F239297%2FgetCode%3Fpp%3Dgiy%26ps%3Dcbgy%26p2%3Dfefs%26pfc%3Da%26pfb%3Da%26plp%3Da%26pli%3Da%26pop%3Da%26dl%3Dseasonvar.ru%26puid1%3D1%26puid22%3D16%26puid6%3D29%26puid33%3D3%26puid8%3D9%26puid11%3D0%26puid12%3D13%7CADVXL:0%7CADREF:nobanner%7CADVPT:124%7CADCLS:0%7CADWRPC:0
+			line2 = urllib.unquote(line1).decode('utf8', 'replace').encode('utf8')
+			split1 = line2.split('|')
+			if len(split1)==3:
+				print(line1)
+				raise NameError('len(split1)=3') 
 
 		fields = {}
-		for k,v in raw_fields.iteritems():
-			try_line = line_to_fields('%s:%s' % (k, URLDecodedField(v).clean(),))
-			fields.update(try_line) 
 
+		# 1489616707.108|84.163.88.44|4.3|A0982149-7FEB-E6EA-EB4F-934DB1F7DCD5||1E2A36EE-A425-8537-D22F-05B52E31AAD9|1|2||F|PLADF||101964442|||https://cdn.pladform.ru|||M|PLDF_PL:18046|ADXREF:https%3A%2F%2Fpubads.g.doubleclick.net%2Fgampad%2Fads%3Fsz%3D640x480%26iu%3D%2F98544404%2FPladformNetwork%26cust_params%3Dcllive%253D2%2526clps%253D3%2526clurl%253D%25%25PATTERN%3Aodnoklassniki.ru%2526clcontentID%253D4m%2526clint%253Dwaterfall%2526clviti%253D%25%25PATTERN%3Avideotitle%25%25%2526clvidesc%253D%25%25PATTERN%3Avideodescription%25%25%2526clcoin%253D1%2526clcoty%253Dnonesl%2526clcogrp%253Dgames%2526clauone%253DPladformNetwork%2526clpos%253Dpre%2526cldevca%253Ddesktop%2526clvimu%253D2%2526clvpaid%253D1%26impl%3Ds%26gdfp_req%3D1%26env%3Dvp%26output%3Dvast%26unviewed_position_start%3D1%26url%3Dodnoklassniki.ru%26description_url%3Dodnoklassniki.ru%26correlator%3D9769302930355203|ADVXL:0|ADREF:nobanner|ADVPT:2298|ADCLS:0|ADWRPC:0 				
+		with_anonymous_fields_client = split1[2][:1].isdigit()
+		k=0
+		for v in split1:
+			field_name = ''
+			field_value = ''							
+			if k < len(self.anonymous_fields_server):
+				field_name = self.anonymous_fields_server[k]
+				field_value = v
+			elif with_anonymous_fields_client and k < len(self.anonymous_fields_server)+len(self.anonymous_fields_client):
+				field_name = self.anonymous_fields_client[k - len(self.anonymous_fields_server)]				
+				field_value = v
+			else:
+				v1 = v.split(":", 1)
+				if len(v1) == 2:
+					field_name = v1[0]				
+					field_value = v1[1] #, k 
+				else:  # 1489616703.683|46.38.48.106|4.3%7C085049C7-8889-9F9F-1424-62F5B6E53B2B%7C%7C2A9328B8-BBCB-E4C1-3585-406573454A6A%7C5%7C129%7C%7CF%7CPLADF%7C%7C100821058%7C%7C%7Chttp://cdn.pladform.ru%7C%7C%7CM%7CADWRPC:0%7CADREF:nobanner%7CADVPT:101%7CADVXL:0%7CADXREF:http%3A%2F%2Fad.mail.ru%2Fvast%2F3930%3Fpuid1%3D13%26puid2%3D301%26puid3%3D2%26puid4%3D1%26puid5%3D16%26puid6%3D29%26puid7%3D3%26puid8%3D9%26puid9%3D1%26puid10%3D1%26puid11%3D0%26puid12%3D13%26dl%3Dhttp%253A%252F%252Fseasonvar.ru%252Fserial-6625-Kohanya-02-sezon.html%26duration%3D1463%26content_id%3D100821058%26eid2%3D13151%26puid18%3D1%26eid1%3D13151%7C109%7C100821058%7C2028%7CADCLS:0%7CPLDF_PL:13151
+					field_name = 'err_log_unknown'
+					if fields.has_key(field_name):
+						field_value = fields[field_name] + '|' + v1[0]
+					else:
+						field_value = v1[0] 
+			fields[field_name] = field_value #, k # fields.update(field_name, field_value)			
+			k = k + 1
 		assert self.mandatory_fields == self.mandatory_fields & set(fields.keys()) , "no_mandatory_fields"
 
-		facts = {}         
-
 		def process_fields():
-			for field in fields:
-				sampling_data_finded = False
+			facts = {}
+			sampling_finded = False	# sampling_passed = True	
+			facts['log_source'] = 0 # TODO: допилить с учетом yast						 
+			for field in fields:			
 				field_from = field
-				if field_from == sampling_data_field:
-					sampling_data_finded = True
-					if len(sampling_data_mask)>0:
-						if sampling_data_mask != facts[field_to][:len(sampling_data_mask)]:
-							return None
+				if field_from == self.sampling_data_field:
+					sampling_finded = True
+					if self.sampling_data_mask_len>0:
+						if self.sampling_data_mask != fields[field_from][:self.sampling_data_mask_len]:
+							return None # sampling_passed = False # None
 				field_to = ''
 				for row in self.fields:
 					if row[0]==field_from:
@@ -335,7 +367,8 @@ class UploadSessionPlayerEvents(UploadSession):
 						field_type = row[2]
 						try:
 							v = (eval('%s(fields[\'%s\'])' % (field_type, field_from, ))).clean()
-							facts[field_to] = v
+							# print(type(v))
+							facts[field_to] = v # eval('%s(fields[\'%s\'])' % (field_type, field_from,)).clean()
 						except:
 							bad_data = field_from + ":" + fields[field_from]
 							if facts.has_key('err_log_bad'):
@@ -344,18 +377,17 @@ class UploadSessionPlayerEvents(UploadSession):
 								facts['err_log_bad'] = bad_data
 							print('err_log_bad: ' + facts['err_log_bad'])
 						break # return #  
-				# print(fields)						 
+				# print(fields)
 				if field_to=='':
 					if facts.has_key('err_log_unknown'):
 						facts['err_log_unknown'] = facts['err_log_unknown'] + '|' + field_from
 					else:
-						facts['err_log_unknown'] = field_from     
-			
-			assert sampling_data_finded, 'no_' + sampling_data
+						facts['err_log_unknown'] = field_from
+			assert sampling_finded, 'no_' + self.sampling_data_field
+			return facts
 
-		process_fields()
-		#print(facts)
-		return facts		
+		return process_fields()	
+		
 
 def main():
 	(prog_options, prog_args) = parse_args()
@@ -373,7 +405,7 @@ def main():
 	for log_filename in log_filenames:
 		with UploadSession(log_filename, prog_options.data_type, prog_options.sampling_data) as log_session:
 			time_start = time.time()
-			print('Started processing file %s' % (log_filename,))
+			print('Started processing file %s, sampling_data - %s' % (log_filename, prog_options.sampling_data))
 
 			log_session.parse()
 
